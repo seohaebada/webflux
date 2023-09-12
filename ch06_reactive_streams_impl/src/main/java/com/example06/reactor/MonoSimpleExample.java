218e7e3c86462818d6f3bb7e70170f1d6b88a73e
seohaebada <seohae0406@gmail.com>
2023-09-12 19:29:55 +0900

    (09.12) [#L012] chapter06 예제코드 준비

---
+++ ch06_reactive_streams_impl/src/main/java/com/example06/reactor/MonoSimpleExample.java
@@ -0,0 +1,24 @@
+package com.example06.reactor;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class MonoSimpleExample {
+    @SneakyThrows
+    public static void main(String[] args) {
+        log.info("start main");
+        getItems()
+                .subscribe(new SimpleSubscriber<>(Integer.MAX_VALUE));
+        log.info("end main");
+
+        Thread.sleep(1000);
+    }
+
+    private static Mono<Integer> getItems() {
+        return Mono.create(monoSink -> {
+            monoSink.success(1);
+        });
+    }
+}
---
+++ ch06_reactive_streams_impl/src/main/java/com/example06/rxjava/CompletableErrorExample.java
@@ -0,0 +1,21 @@
+package com.example06.rxjava;
+
+import io.reactivex.rxjava3.core.Completable;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CompletableErrorExample {
+    public static void main(String[] args) {
+        getCompletion()
+                .subscribe(new SimpleCompletableObserver());
+    }
+
+    private static Completable getCompletion() {
+        return Completable.create(completableEmitter -> {
+            Thread.sleep(1000);
+            completableEmitter.onError(
+                    new RuntimeException("error in completable")
+            );
+        });
+    }
+}
