e0369777f387453e80ca050857f3f88a14bb5105
seohaebada <seohae0406@gmail.com>
2023-10-09 20:33:40 +0900

    (10.09) [#L012] part2 - chapter05 webflux 심화 - init

---
+++ part2/ch05_webflux_2/src/main/java/com/example2_05/Ch05Webflux2Application.java
@@ -0,0 +1,13 @@
+package com.example2_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch05Webflux2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch05Webflux2Application.class, args);
+    }
+
+}
---
+++ part2/ch05_webflux_2/src/test/java/com/example2_05/Ch05Webflux2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example2_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch05Webflux2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
