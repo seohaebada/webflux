c7e23b913f7336284248d27d33fd5e584ecb19eb
seohaebada <seohae0406@gmail.com>
2023-10-27 20:01:48 +0900

    (10.27) [#L012] part2 - chapter07 준비

---
+++ part2/ch07_websocket/src/main/java/com/example/ch07_websocket/Ch07WebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch07_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch07WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch07WebsocketApplication.class, args);
+    }
+
+}
---
+++ part2/ch07_websocket/src/test/java/com/example/ch07_websocket/Ch07WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch07_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch07WebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
